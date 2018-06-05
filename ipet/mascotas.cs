79785379c28bf794b6890e6e3f19a214076313b5
Alumno <Alumno@WINDOWS7-PC>
2018-06-05 14:22:43 +0200

    pruebita

--- ipet/mascotas.cs
+++ ipet/mascotas.cs
@@ -11,18 +11,18 @@ using System.Data.SqlClient;
 using System.Configuration;
 //
 //
-/// <summary>
-/// comentar metodos
-/// </summary>
+
 namespace ipet
 {
 	public partial class Mascotas : Form
 	{
 		private List<DataRow> rows;
+		
 		public Mascotas()
 		{
 			InitializeComponent();
 			LoadListViewMascotas();
+			
 		}
 
 		private void label6_Click(object sender, EventArgs e)
@@ -39,6 +39,7 @@ namespace ipet
 			SqlConnection conn = new SqlConnection(settings.ConnectionString);
 			SqlCommand cmdMostrarmas =new SqlCommand( "ProyectoFinal.dbo.pa_mostrarMas", conn);
 			cmdMostrarmas.CommandType = CommandType.StoredProcedure;
+			
 
 			try
 			{
@@ -48,18 +49,19 @@ namespace ipet
 				SqlDataReader rdr = cmdMostrarmas.ExecuteReader();
 				DataTable dt = new DataTable();
 
+
 				dt.Load(rdr); //Cargando una tabla donde le pasamos el DataReader
 				rows = dt.AsEnumerable().ToList();
-
+				
 				ListView.ListViewItemCollection listam = new ListView.ListViewItemCollection(lista_mascotas);
 
 				foreach (DataRow m in rows)
 				{
 					
-					listam.Add(m[0].ToString(),0);
+					listam.Add(m["NOMBRE"].ToString(),0);
 					
 				}
-
+				
 			}
 			
 
@@ -79,23 +81,43 @@ namespace ipet
 			fechanac.Text = n["FechaNac"].ToString();
 			ciudad.Text = n["CIUDAD"].ToString();
 			tamaño.Text = n["tamaño"].ToString();
-
-
+			foto.Image = Image.FromFile(System.IO.Path.Combine(Application.StartupPath, n["imagen"].ToString()));
 
 		}
-
-	private void lista_mascotas_SelectedIndexChanged(object sender, EventArgs e)
+		private void LoadMascotaSeleccionadaAtributo(List<DataRow> atributos)
+		{
+			
+			if (atributos.Count == 1)
 			{
-
-			if (lista_mascotas.SelectedItems.Count > 0)
+				atributo1.Text = atributos[0][0].ToString();
+				valor1.Text = atributos[0][1].ToString();
+			}
+			if (atributos.Count == 2)
 			{
-
-				DataRow n = rows.ElementAt(lista_mascotas.SelectedIndices[0]);
-				LoadMascotaSeleccionada(n);
-
+				atributo1.Text = atributos[0][0].ToString();
+				atributo2.Text = atributos[1][0].ToString();
+				valor1.Text = atributos[0][1].ToString();
+				valor2.Text = atributos[1][1].ToString();
 			}
 
 		}
+		private void lista_mascotas_SelectedIndexChanged(object sender, EventArgs e)
+			{
+			
+
+				if (lista_mascotas.SelectedItems.Count > 0)
+				{
+
+					DataRow n = rows.ElementAt(lista_mascotas.SelectedIndices[0]);
+
+					LoadMascotaSeleccionada(n);
+					LoadMascotaSeleccionadaAtributo(mascota.LoadAtributosxMascota(Convert.ToInt16(n["IDMascota"])));
+
+				}
+
+			}
+
+		
 
 
 
@@ -228,7 +250,44 @@ namespace ipet
 				MessageBox.Show("No se ha podido conectar " + ex.Message);
 			}
 		}
-		
 
+		private void reservar_Click(object sender, EventArgs e)
+		{
+			//LEER ARCHIVO CONFIGURACION
+			ConnectionStringSettings settings =
+				ConfigurationManager.ConnectionStrings["ProyectoFinalConnectionString"];
+			//conectar con bbd
+			SqlConnection conn = new SqlConnection(settings.ConnectionString);
+			SqlCommand cmdNuevoAdsp = new SqlCommand("ProyectoFinal.dbo.pa_reservar", conn);
+			cmdNuevoAdsp.CommandType = CommandType.StoredProcedure;
+
+			DataRow n = rows.ElementAt(lista_mascotas.SelectedIndices[0]);
+
+			cmdNuevoAdsp.Parameters.Add(new SqlParameter("@id", SqlDbType.Int));
+			cmdNuevoAdsp.Parameters["@id"].Value = Convert.ToInt16(n["IDMascota"]);
+
+
+
+			try
+			{
+				//abrir la conexion
+				conn.Open();
+
+				cmdNuevoAdsp.ExecuteNonQuery();
+				
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("No se ha podido conectar " + ex.Message);
+
+			}
+
+
+		}
+
+		private void foto_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
